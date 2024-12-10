38ce2fcfa81d251633045e79925e5cd3304cf6ee
Ina F. Pedersen <inapedersen90@gmail.com>
2024-12-10 20:06:06 +0100

    feat add assets

---
+++ Assets/Script/AnimationController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimationController : MonoBehaviour
+{
+    //Variables:
+    public Animator animator;
+    public Transform girlModel;
+    public Transform pauseButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = girlModel.GetComponent<Animator>();
+    }
+
+    public void PlayGreetingAnim()
+    {
+        animator.Play("Greeting");
+    }
+
+    public void PlayWalkingAnim()
+    {
+        animator.Play("Walking");
+    }
+
+    public void PlayJumpingAnim()
+    {
+        animator.Play("Jumping");
+    }
+
+    public void PlayLayingAnim()
+    {
+        animator.Play("Laying");
+    }
+
+    public void PlaySwimmingAnim()
+    {
+        animator.Play("Swimming");
+    }
+
+    public void PlayClimbingAnim()
+    {
+        animator.Play("Climbing");
+    }
+
+    public void PlaySingingAnim()
+    {
+        animator.Play("Singing");
+    }
+
+    public void PlayBowingAnim()
+    {
+        animator.Play("Bowing");
+    }
+
+
+    public void PauseAnimation()
+    {
+        animator.speed = 0;
+        Button btn = pauseButton.GetComponent<Button>();
+        btn.onClick.AddListener(ResumeAnimation);
+    }
+
+    void ResumeAnimation()
+    {
+        animator.speed = 1;
+        Button btn = pauseButton.GetComponent<Button>();
+        btn.onClick.AddListener(PauseAnimation);
+    }
+}
---
+++ Assets/Script/ModelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelManager : MonoBehaviour
+{
+    public void ScaleModel()
+    {
+        transform.localScale += new Vector3(0.01f, 0.01f, 0.01f);
+    }
+
+    public void ScaleDownModel()
+    {
+        transform.localScale -= new Vector3(0.01f, 0.01f, 0.01f);
+    }
+
+    public void RotateModel()
+    {
+        transform.Rotate(new Vector3(0, 20f, 0));
+    }
+}
